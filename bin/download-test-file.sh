f2108335938b3e770a9c2cd929cc8a6da5344d65
Jared Allard <jaredallard@outlook.com>
2019-07-03 15:51:32 -0700

    chore: MINIO to converter, faster local encoding settings, update download script

--- bin/download-test-file.sh
+++ bin/download-test-file.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # Download the test file
 
+if [[ -e "/tmp/Bunny.mkv" ]]; then
+  rm -rf /tmp/Bunny.mkv
+fi
+
 wget -O /tmp/Bunny.mkv https://s3.amazonaws.com/x265.org/video/BigBuckBunny_2000hevc.mp4
